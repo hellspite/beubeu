f7e69ee1126f7feb7244f740cee1c0f95b1aa89d
Emiliano Nobis <emiliano.nobis@gmail.com>
2018-02-03 18:58:39 +0100

    Installato vich uploader

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
