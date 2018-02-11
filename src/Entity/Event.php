67c045c0a82d871eb1c1cba08809dffb0677a744
Emiliano Nobis <emiliano.nobis@gmail.com>
2018-02-11 18:00:31 +0100

    Backend Eventi

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -4,9 +4,14 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use App\Entity\News;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\EventRepository")
+ * @ORM\HasLifecycleCallbacks()
+ * @Vich\Uploadable
  */
 class Event
 {
@@ -26,6 +31,12 @@ class Event
     * @ORM\Column(type="string", length=250) 
     */
     private $image;
+    
+    /**
+    * @Vich\UploadableField(mapping="event_images", fileNameProperty="image")
+    * @var File
+    */
+    private $imageFile;
 
     /**
     * @ORM\Column(type="text") 
@@ -49,6 +60,12 @@ class Event
     */
     private $created;
 
+    /**
+    * @ORM\Column(type="datetime", nullable=true)
+    * @var \DateTime
+    */
+    private $updated;
+
     public function getId(){
         return $this->id;
     }
@@ -67,6 +84,18 @@ class Event
 
     public function setImage($image){
         $this->image = $image;
+    }
+
+    public function getImageFile(){
+        return $this->imageFile;
+    }
+
+    public function setImageFile(File $image = null){
+        $this->imageFile = $image;
+
+        if($image){
+            $this->updated = new \DateTime('now');
+        }
     }
 
     public function getDescription(){
@@ -91,5 +120,20 @@ class Event
 
     public function getCreated(){
         return $this->created;
+    }
+
+    /**
+    * @ORM\PostPersist
+    */
+    public function createNews($args){
+
+        $em = $args->getEntityManager();
+        $news = new News();    
+        $news->setSourceId($this->getId());
+        $news->setSource('Event');
+
+        $em->persist($news);
+        $em->flush();
+
     }
 }
