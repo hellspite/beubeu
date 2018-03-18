aeb4809e384bc6e649c47a6415f9c9f467081af5
Emiliano Nobis <emiliano.nobis@gmail.com>
2018-03-18 17:28:20 +0100

    Creati campi galleria per gli eventi

---
+++ src/Migrations/Version20180318162752.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180318162752 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event ADD gallery1 VARCHAR(250) DEFAULT NULL, ADD gallery2 VARCHAR(250) DEFAULT NULL, ADD gallery3 VARCHAR(250) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event DROP gallery1, DROP gallery2, DROP gallery3');
+    }
+}
