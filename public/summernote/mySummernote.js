62a23b448895657bba245148a292773a200a5c27
Emiliano Nobis <emiliano.nobis@gmail.com>
2018-08-11 12:30:17 +0200

    Aggiunto font Simoncini Garamond all'editor

--- public/summernote/mySummernote.js
+++ public/summernote/mySummernote.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $('textarea').summernote({
         height: 300,
-        lang: 'it-IT'
+        lang: 'it-IT',
+        fontNames: ['Simoncini Garamond','Arial', 'Arial Black', 'Comic Sans MS', 'Courier New'],
+        fontNamesIgnoreCheck: ['Simoncini Garamond'],
     });
 });
